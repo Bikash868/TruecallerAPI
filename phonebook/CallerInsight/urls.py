649f8823af676c68b6d06e531bf062b4a1e8561e
Bikash Das <ursbikash868@gmail.com>
2024-07-29 01:25:59 +0530

    feat: Added search by name and search by phone number functionality

--- phonebook/CallerInsight/urls.py
+++ phonebook/CallerInsight/urls.py
@@ -3,5 +3,8 @@ from CallerInsight.views import *
 
 urlpatterns=[
 	path('register/',Register.as_view(),name='register'),
-	path('login/',Login.as_view(),name='login'),	
+	path('login/',Login.as_view(),name='login'),
+
+	path('search_by_name/',SearchByName.as_view(),name='search_by_name'),
+	path('search_by_phone_number/',SearchByPhoneNumber.as_view(),name='search_by_phone_number'),	
 ]
